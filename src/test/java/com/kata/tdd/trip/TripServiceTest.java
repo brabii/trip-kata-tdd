9f900bd8d971e6c72af13fd2c19ebfc23e92a7af
brabii <collectorsgarbages@gmail.com>
2021-05-22 14:55:14 +0200

    maj test shouldThrowExceptionWhenUserNotLoggedIn

--- src/test/java/com/kata/tdd/trip/TripServiceTest.java
+++ src/test/java/com/kata/tdd/trip/TripServiceTest.java
@@ -7,17 +7,22 @@ import com.kata.tdd.user.User;
 
 public class TripServiceTest {
 
+	private static final User GUEST = null;
+	private static final User UNUSED_USER = null;
+	private User loggedInUser;
+
 	@Test(expected = UserNotLoggedInException.class)
 	public void shouldThrowExecptionWhenUserNotLoggedIn() {
 		TripService tripService = new TestableTripService();
-		tripService.getTripsByUser(null);
+		loggedInUser = GUEST;
+		tripService.getTripsByUser(UNUSED_USER);
 	}
 
 	private class TestableTripService extends TripService {
 
 		@Override
 		protected User getLoggedUser() {
-			return null;
+			return loggedInUser;
 		}
 	}
 }
