18285b2678fdfe6f1862ba291506a4b675853803
brabii <collectorsgarbages@gmail.com>
2021-05-22 15:33:04 +0200

    ajout test should_not_return_any_trips_if_not_friends

--- src/test/java/com/kata/tdd/trip/TripServiceTest.java
+++ src/test/java/com/kata/tdd/trip/TripServiceTest.java
@@ -1,5 +1,10 @@
 package com.kata.tdd.trip;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import com.kata.tdd.exception.UserNotLoggedInException;
@@ -9,13 +14,32 @@ public class TripServiceTest {
 
 	private static final User GUEST = null;
 	private static final User UNUSED_USER = null;
+	private static final User PAUL = new User();
+	private static final User TERRY = new User();
+	private static final User REGISTERED_USER = new User();
+	private static final Trip TO_BRAZIL = new Trip();
 	private User loggedInUser;
+	TripService tripService;
+
+	@Before
+	public void initialise() {
+		tripService = new TestableTripService();
+	}
 
 	@Test(expected = UserNotLoggedInException.class)
 	public void shouldThrowExecptionWhenUserNotLoggedIn() {
-		TripService tripService = new TestableTripService();
 		loggedInUser = GUEST;
 		tripService.getTripsByUser(UNUSED_USER);
+	}
+
+	@Test
+	public void shouldNotReturnAnyTripsWhenUsersAreNotFriends() {
+		loggedInUser = REGISTERED_USER;
+		PAUL.addFriend(TERRY);
+		PAUL.addTrip(TO_BRAZIL);
+		TripService tripService = new TestableTripService();
+		List<Trip> trips = tripService.getTripsByUser(PAUL);
+		assertTrue(trips.size() == 0);
 	}
 
 	private class TestableTripService extends TripService {
